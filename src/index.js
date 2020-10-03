b50646125bb9630720bbf8acb5549f2f9d9d61b4
T0XTER <tyutyunnik.serg@gmail.com>
2020-10-03 22:02:23 +0300

    feat: add function min

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length === 0 || array.length === 0 ){
+        return 0
+    } else {
+        let i;
+        let min = array[0];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i]
+            }
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return 0;
 }
